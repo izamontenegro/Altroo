d66db0182203df1c5df6a7f0e9c5affa2d9ddd07
Layza Carneiro <layza.mrcarneiro@gmail.com>
2025-10-02 23:34:56 -0300

    test: add unit and integration tests for CareRecipientFacade MentalState extension

---
+++ AltrooTests/Services/CareRecipient+MentalStateTest.swift
@@ -0,0 +1,144 @@
+//
+//  CareRecipient+MentalStateTest.swift
+//  Altroo
+//
+//  Created by Layza Maria Rodrigues Carneiro on 02/10/25.
+//
+
+import CoreData
+import XCTest
+@testable import Altroo
+
+// MARK: - CoreDataService Mock
+final class CoreDataServiceMock: CoreDataService {
+    var saveCalledCount = 0
+    
+    override func save() {
+        saveCalledCount += 1
+    }
+}
+
+// MARK: - Mocks for the protocols that CareRecipientFacade asks
+class BasicNeedsFacadeMock: BasicNeedsFacadeProtocol {}
+class RoutineActivitiesFacadeMock: RoutineActivitiesFacadeProtocol {}
+
+class CareRecipientFacadeTests: XCTestCase {
+    
+    var sut: CareRecipientFacade!
+    var persistenceMock: CoreDataServiceMock!
+    var context: NSManagedObjectContext!
+    var mentalState: MentalState!
+    
+    override func setUp() {
+        super.setUp()
+        
+        // Core Data stack in-memory
+        let container = NSPersistentContainer(name: "AltrooDataModel")
+        let description = NSPersistentStoreDescription()
+        description.type = NSInMemoryStoreType
+        container.persistentStoreDescriptions = [description]
+        container.loadPersistentStores { _, error in
+            XCTAssertNil(error)
+        }
+        context = container.viewContext
+        
+        mentalState = MentalState(context: context)
+        
+        // Mocks
+        persistenceMock = CoreDataServiceMock()
+        sut = CareRecipientFacade(
+            basicNeedsFacade: BasicNeedsFacadeMock(),
+            routineActivitiesFacade: RoutineActivitiesFacadeMock(),
+            persistenceService: persistenceMock
+        )
+
+    }
+    
+    override func tearDown() {
+        sut = nil
+        persistenceMock = nil
+        mentalState = nil
+        context = nil
+        super.tearDown()
+    }
+
+    // MARK: - Unit Tests
+    func test_addEmotionalState_updatesProperty_andCallsSave() {
+        sut.addEmotionalState(emotionalState: .calm, mentalState: mentalState)
+        
+        XCTAssertEqual(mentalState.emotionalState, EmotionalStateEnum.calm.rawValue)
+        XCTAssertEqual(persistenceMock.saveCalledCount, 1)
+    }
+    
+    func test_addMemoryState_updatesProperty_andCallsSave() {
+        sut.addMemoryState(memoryState: .frequentForgetfulness, mentalState: mentalState)
+        
+        XCTAssertEqual(mentalState.memoryState, MemoryEnum.frequentForgetfulness.rawValue)
+        XCTAssertEqual(persistenceMock.saveCalledCount, 1)
+    }
+    
+    func test_addOrientationState_updatesProperty_andCallsSave() {
+        sut.addOrientationState(orientationState: .disorientedInTime, mentalState: mentalState)
+        
+        XCTAssertEqual(mentalState.orientationState, OrientationEnum.disorientedInTime.rawValue)
+        XCTAssertEqual(persistenceMock.saveCalledCount, 1)
+    }
+    
+    func test_addCognitionState_updatesProperty_andCallsSave() {
+        sut.addCognitionState(cognitionState: .mediumCapacity, mentalState: mentalState)
+        
+        XCTAssertEqual(mentalState.cognitionState, CognitionEnum.mediumCapacity.rawValue)
+        XCTAssertEqual(persistenceMock.saveCalledCount, 1)
+    }
+    
+    func test_multipleUpdates_shouldAccumulateSaveCalls() {
+        sut.addEmotionalState(emotionalState: .anxious, mentalState: mentalState)
+        sut.addMemoryState(memoryState: .intact, mentalState: mentalState)
+        
+        XCTAssertEqual(mentalState.emotionalState, EmotionalStateEnum.anxious.rawValue)
+        XCTAssertEqual(mentalState.memoryState, MemoryEnum.intact.rawValue)
+        XCTAssertEqual(persistenceMock.saveCalledCount, 2)
+    }
+    
+    func test_updateAllMentalStates_together() {
+        sut.addEmotionalState(emotionalState: .aggressive, mentalState: mentalState)
+        sut.addMemoryState(memoryState: .frequentForgetfulness, mentalState: mentalState)
+        sut.addOrientationState(orientationState: .disorientedInAll, mentalState: mentalState)
+        sut.addCognitionState(cognitionState: .lowCapacity, mentalState: mentalState)
+        
+        XCTAssertEqual(mentalState.emotionalState, EmotionalStateEnum.aggressive.rawValue)
+        XCTAssertEqual(mentalState.memoryState, MemoryEnum.frequentForgetfulness.rawValue)
+        XCTAssertEqual(mentalState.orientationState, OrientationEnum.disorientedInAll.rawValue)
+        XCTAssertEqual(mentalState.cognitionState, CognitionEnum.lowCapacity.rawValue)
+        XCTAssertEqual(persistenceMock.saveCalledCount, 4)
+    }
+
+    
+    // MARK: - Integration test with core data
+    func test_addCognitionState_persistsToCoreData() {
+        sut.addCognitionState(cognitionState: .mediumCapacity, mentalState: mentalState)
+        try? context.save()
+        
+        let fetch: NSFetchRequest<MentalState> = MentalState.fetchRequest()
+        let results = try? context.fetch(fetch)
+        
+        XCTAssertEqual(results?.first?.cognitionState, CognitionEnum.mediumCapacity.rawValue)
+    }
+    
+    func test_addAllMentalStates_together_persistsToCoreData() {
+        sut.addEmotionalState(emotionalState: .lively, mentalState: mentalState)
+        sut.addMemoryState(memoryState: .intact, mentalState: mentalState)
+        sut.addOrientationState(orientationState: .disorientedInSpace, mentalState: mentalState)
+        sut.addCognitionState(cognitionState: .mediumCapacity, mentalState: mentalState)
+
+        try? context.save()
+        
+        let fetch: NSFetchRequest<MentalState> = MentalState.fetchRequest()
+        let results = try? context.fetch(fetch)
+        
+        XCTAssertEqual(results?.first?.emotionalState, EmotionalStateEnum.lively.rawValue)
+        XCTAssertEqual(results?.first?.memoryState, MemoryEnum.intact.rawValue)
+        XCTAssertEqual(results?.first?.orientationState, OrientationEnum.disorientedInSpace.rawValue)
+        XCTAssertEqual(results?.first?.cognitionState, CognitionEnum.mediumCapacity.rawValue)
+    }
+}
