04341c05441c7206802b97d007b22cbc03b657d8
RaissaParente <raissabparente@gmail.com>
2025-10-09 16:22:26 -0300

    feat: helper classes for forms and layout

---
+++ Altroo/Core/Models/Enums/LayoutEnums.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutEnums.swift
+//  Altroo
+//
+//  Created by Raissa Parente on 09/10/25.
+//
+import Foundation
+
+enum Layout {
+    static let verySmallSpacing = 6
+    static let smallSpacing: CGFloat = 8 //top card padding
+    static let standardSpacing: CGFloat = 12 //side card padding
+    static let mediumSpacing: CGFloat = 16 //side view padding, in between components
+    static let largeSpacing: CGFloat = 22
+    static let verylargeSpacing: CGFloat = 24 //in between components
+    static let hugeSpacing: CGFloat = 30 //home section spacing
+}
