ae4eca973b84c433ef382f52c2526b2a0dae5880
maarcq <marcellerq27@gmail.com>
2025-10-13 01:04:15 -0300

    feat: I created the StandardTextfield component that can pass a height, width, title and placeholder to the views that need to use it.

--- Altroo/Shared/Components/FormControls/StandardTextfield.swift
+++ Altroo/Shared/Components/FormControls/StandardTextfield.swift
@@ -6,33 +6,86 @@
 //
 import UIKit
 
-class StandardTextfield: UITextField {
-    init() {
+class StandardTextfield: UIView {
+    
+    var textfieldWidth: CGFloat
+    var textfieldHeight: CGFloat
+    var textfieldTitle: StandardLabel!
+    var textfieldPlaceholder: String
+    
+    let textField = UITextField()
+    
+    init(width: CGFloat, height: CGFloat, title: StandardLabel, placeholder: String) {
+        self.textfieldWidth = width
+        self.textfieldHeight = height
+        self.textfieldTitle = title
+        self.textfieldPlaceholder = placeholder
+        
         super.init(frame: .zero)
-        borderStyle = .roundedRect
-        backgroundColor = UIColor(resource: .teal80)
-        font = UIFont.systemFont(ofSize: 18)
-        placeholder = "Maria Clara"
+        
+        setupView()
     }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    // MARK: - Setup
+    private func setupView() {
+        // Title label
+        textfieldTitle = StandardLabel(
+            labelText: "Nome",
+            labelFont: .sfPro,
+            labelType: .callOut,
+            labelColor: .black10,
+            labelWeight: .regular
+        )
+        textfieldTitle.textAlignment = .left
+        textfieldTitle.numberOfLines = 0
+        textfieldTitle.lineBreakMode = .byWordWrapping
+        textfieldTitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        // TextField
+        textField.placeholder = textfieldPlaceholder
+        textField.textAlignment = .left
+        textField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        textField.borderStyle = .roundedRect
+        textField.textColor = UIColor(resource: .black10)
+        textField.backgroundColor = UIColor(resource: .white70)
+        textField.widthAnchor.constraint(equalToConstant: textfieldWidth).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: textfieldHeight).isActive = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(textfieldTitle)
+        addSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            textfieldTitle.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: -8),
+            textField.topAnchor.constraint(equalTo: textfieldTitle.bottomAnchor, constant: 8)
+        ])
+    }
 }
 
-
-
-//MARK: PREVIEW
 import SwiftUI
+
 private struct TextfieldPreviewWrapper: UIViewRepresentable {
-    func updateUIView(_ uiView: StandardTextfield, context: Context) {
-    }
+    func updateUIView(_ uiView: StandardTextfield, context: Context) { }
     
     func makeUIView(context: Context) -> StandardTextfield {
-        return StandardTextfield()
+        return StandardTextfield(
+            width: 370,
+            height: 38,
+            title: StandardLabel(labelText: "Nome",
+                                 labelFont: .sfPro,
+                                 labelType: .title3,
+                                 labelColor: UIColor(resource: .black10),
+                                 labelWeight: .medium
+                                ),
+            placeholder: "Maria Clara"
+        )
     }
 }
 
 #Preview {
     TextfieldPreviewWrapper()
+        .padding()
 }
-
--- Altroo/Shared/Components/PreviewComponents.swift
+++ Altroo/Shared/Components/PreviewComponents.swift
@@ -56,9 +56,19 @@ class ComponentPreviewViewController: UIViewController {
 
     private func previewComponents() {
         // TEXTFIELD
-        let textField = StandardTextfield()
-        textField.placeholder = "Digite algo..."
-        textField.borderStyle = .roundedRect
+        let textField = StandardTextfield(
+            width: 250,
+            height: 45,
+            title: StandardLabel(labelText: "Nome",
+                                 labelFont: .sfPro,
+                                 labelType: .title3,
+                                 labelColor: UIColor(resource: .black10),
+                                 labelWeight: .medium
+                                ),
+            placeholder: "Maria Clara"
+        )
+//        textField.placeholder = "Digite algo..."
+//        textField.borderStyle = .roundedRect
 
         // SEGMENTEDCONTROL
         let segmentedControl = StandardSegmentedControl(items: ["Opção 1", "Opção 2", "Opção 3"])
@@ -83,7 +93,7 @@ class ComponentPreviewViewController: UIViewController {
         //CAPSULES
         //these are views, not buttons
         //to make a capsule button add it as the button's view
-        let capsule1 = CapsuleWithCircleView(iconName: "pencil", text: "Editar Seções", mainColor: UIColor(resource: .teal80), accentColor: UIColor(resource: .teal20))
+//        let capsule1 = CapsuleWithCircleView(iconName: "pencil", text: "Editar Seções", mainColor: UIColor(resource: .teal80), accentColor: UIColor(resource: .teal20))
         let capsule2 = CapsuleIconView(iconName: "drop.fill", text: "250ml")
         
         stackView.addArrangedSubview(textField)
@@ -95,7 +105,7 @@ class ComponentPreviewViewController: UIViewController {
         stackView.addArrangedSubview(button4)
         stackView.addArrangedSubview(button5)
         stackView.addArrangedSubview(button6)
-        stackView.addArrangedSubview(capsule1)
+//        stackView.addArrangedSubview(capsule1)
         stackView.addArrangedSubview(capsule2)
     }
 }
