804b5c86d7467b585bab6409d8fd3f918f93633e
Izadora Montenegro <125757162+izamontenegro@users.noreply.github.com>
2025-10-16 11:15:12 -0300

    feat: add the urine colors tho the asset catalog

--- Altroo/Features/Main/Today/Views/BasicNeeds/UrineRecordViewController.swift
+++ Altroo/Features/Main/Today/Views/BasicNeeds/UrineRecordViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class UrineRecordViewController: UIViewController {
+class UrineRecordViewController: GradientNavBarViewController {
+    
+    let urineColors = [UIColor.urineLight, UIColor.urineLightYellow, UIColor.urineYellow, UIColor.urineOrange, UIColor.urineRed]
 
     let viewLabel: UILabel = {
         let label = UILabel()
@@ -17,16 +19,52 @@ class UrineRecordViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .yellow
         
         view.addSubview(viewLabel)
+
+        setupLayout()
         
         NSLayoutConstraint.activate([
             viewLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             viewLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
+    
+    private func configureUrineColorSection() -> UIView {
+        let urineColorSectionTitle = StandardLabel(labelText: "Cor da Urina?", labelFont: .sfPro, labelType: .callOut, labelColor: .black10, labelWeight: .semibold)
+        
+        let urineColorRow = UIStackView()
+        urineColorRow.axis = .horizontal
+        urineColorRow.spacing = 18
+        
+        for urineColor in urineColors {
+            let circleColor = UIView()
+            circleColor.layer.cornerRadius = 30
+            circleColor.tintColor = urineColor
+            
+            urineColorRow.addArrangedSubview(circleColor)
+        }
+        
+        let urineColorSection = UIStackView()
+        urineColorSection.axis = .vertical
+        urineColorSection.spacing = 16
+        
+        urineColorSection.addArrangedSubview(urineColorSectionTitle)
+        urineColorSection.addArrangedSubview(urineColorRow)
+
+        return urineColorSection
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
+
+#Preview {
+    UrineRecordViewController()
 }
