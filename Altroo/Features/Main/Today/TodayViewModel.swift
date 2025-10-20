f8e64ef9f897ee56b7b99bed3597388e84441042
Layza Carneiro <layza.mrcarneiro@gmail.com>
2025-10-20 06:56:31 -0300

    feat: add functions to fetch urine quantity, stool quantity and tasks

--- Altroo/Features/Main/Today/TodayViewModel.swift
+++ Altroo/Features/Main/Today/TodayViewModel.swift
@@ -5,26 +5,31 @@
 //  Created by Raissa Parente on 13/10/25.
 //
 import Combine
+import Foundation
 
 class TodayViewModel {
     let careRecipientFacade: CareRecipientFacade
     let userService: UserServiceProtocol
-
+    var taskService: RoutineActivitiesFacadeProtocol
+    
     var currentCareRecipient: CareRecipient?
     
     @Published var todaySymptoms: [Symptom] = []
+    @Published var periodTasks: [TaskInstance] = []
     
-    init(careRecipientFacade: CareRecipientFacade, userService: UserServiceProtocol) {
+    @Published var todayStoolQuantity: Int = 0
+    @Published var todayUrineQuantity: Int = 0
+    
+    init(careRecipientFacade: CareRecipientFacade, userService: UserServiceProtocol, taskService: RoutineActivitiesFacadeProtocol) {
         self.careRecipientFacade = careRecipientFacade
         self.userService = userService
-
-//        //FIXME: Change to real patient
-//        self.currentCareRecipient = CoreDataService(stack: CoreDataStack.shared)
-//            .fetchAllCareRecipients()
-//            .first(where: { $0.personalData?.name == "Mrs. Parente" })!
+        self.taskService = taskService
         
         fetchCareRecipient()
         fetchAllTodaySymptoms()
+        fetchAllPeriodTasks()
+        fetchUrineQuantity()
+        fetchStoolQuantity()
     }
     
     private func fetchCareRecipient() {
@@ -35,4 +40,52 @@ class TodayViewModel {
         todaySymptoms = careRecipientFacade.fetchAllSymptomForDate(.now, from: currentCareRecipient!)
     }
     
+    func fetchAllPeriodTasks() {
+        guard let currentCareRecipient else { return }
+        
+        taskService.generateInstancesForToday(for: currentCareRecipient)
+        let allTasks = taskService.fetchAllInstanceRoutineTasks(from: currentCareRecipient)
+        
+        let currentPeriod = PeriodEnum.current
+        let today = Date()
+        let todayWeekday = Locale.Weekday.from(calendarWeekday: Calendar.current.component(.weekday, from: today))
+        
+        periodTasks = allTasks.filter { task in
+            guard let start = task.template?.startDate else { return false }
+            let end = task.template?.endDate
+            
+            let isTodayInInterval = start <= today && (end == nil || end! >= today)
+            let isTodayWeekday = task.template?.weekdays.contains(todayWeekday ?? .monday) ?? false
+            let isCurrentPeriod = task.period == currentPeriod
+            
+            return isTodayInInterval && isTodayWeekday && isCurrentPeriod
+        }
+        .sorted(by: { $0.time ?? .distantPast < $1.time ?? .distantPast })
+    }
+    
+    func fetchUrineQuantity() {
+        guard let currentCareRecipient = currentCareRecipient else { return }
+
+        let calendar = Calendar.current
+        let today = Date()
+        
+        let todayUrine = currentCareRecipient.basicNeeds?.urine?.filter { urineRecord in
+            calendar.isDate((urineRecord as AnyObject).date ?? Date(), inSameDayAs: today)
+        }
+        
+        todayUrineQuantity = todayUrine?.count ?? 0
+    }
+    
+    func fetchStoolQuantity() {
+        guard let currentCareRecipient = currentCareRecipient else { return }
+
+        let calendar = Calendar.current
+        let today = Date()
+        
+        let todayStool = currentCareRecipient.basicNeeds?.urine?.filter { stoolRecord in
+            calendar.isDate((stoolRecord as AnyObject).date ?? Date(), inSameDayAs: today)
+        }
+        
+        todayStoolQuantity = todayStool?.count ?? 0
+    }
 }
