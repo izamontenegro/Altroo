b33d02569d9f011f9d47898030095143bf47a2d2
maarcq <marcellerq27@gmail.com>
2025-11-02 15:47:44 -0300

    feat: I created the animated loading screen.

---
+++ Altroo/Features/Loading/LoadingView.swift
@@ -0,0 +1,100 @@
+//
+//  LoadingView.swift
+//  Altroo
+//
+//  Created by Marcelle Ribeiro Queiroz on 02/11/25.
+//
+
+import SwiftUI
+
+enum loadingPhases: CaseIterable {
+    case ld1, ld2, ld3, ld4, ld5, ld6 ,ld7, ld8, ld9, ld10
+    
+    var text: String {
+        switch self {
+        case .ld1: return "Separando os medicamentos com carinho..."
+        case .ld2: return "Organizando a rotina de cuidados..."
+        case .ld3: return "Anotando os sinais importantes..."
+        case .ld4: return "Verificando se está tudo bem por aí..."
+        case .ld5: return "Preparando o melhor cuidado possível..."
+        case .ld6: return "Checando lembretes e tarefas do dia..."
+        case .ld7: return "Reunindo informações para facilitar seu dia..."
+        case .ld8: return "Configurando lembrete para o remédio de hoje..."
+        case .ld9: return "Verificando os detalhes para você cuidar de quem ama..."
+        case .ld10: return "Deixando tudo pronto para a próxima atividade..."
+        }
+    }
+}
+
+struct LoadingView: View {
+    
+    @State private var showText = false
+    @State private var loading = false
+    @State private var brightness = false
+    @State private var currentPhase: loadingPhases = .ld1
+    
+    var body: some View {
+        VStack(spacing: 32) {
+            Text("Gerando tela personalizada")
+                .foregroundStyle(.pureWhite)
+                .font(Font.custom("Comfortaa", size: 36))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+                .opacity(showText ? 1 : 0)
+                .offset(y: showText ? 0 : 10)
+                .animation(.easeOut(duration: 1.5), value: showText)
+            
+            ZStack {
+                Image("loading1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 140, height: 140)
+                    .offset(x: loading ? 0 : -UIScreen.main.bounds.width)
+                    .animation(.smooth(duration: 2), value: loading)
+                Image("loading2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 140, height: 140)
+                    .offset(x: loading ? 0 : UIScreen.main.bounds.width)
+                    .animation(.smooth(duration: 2), value: loading)
+            }
+            
+            Text(currentPhase.text)
+                .foregroundStyle(.pureWhite)
+                .font(Font.custom("Comfortaa", size: 24))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 40)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .overlay(alignment: .bottom) {
+            Rectangle()
+                .fill(.blue50)
+                .animation(.easeOut(duration: 1).repeatForever(autoreverses: true),
+                           value: brightness)
+                .blur(radius: brightness ? 70 : 100)
+                .frame(width: UIScreen.main.bounds.width, height: brightness ? 130 : 90)
+        }
+        .ignoresSafeArea()
+        .background {
+            Color.blue20
+                .ignoresSafeArea(edges: .all)
+        }
+        .onAppear {
+            currentPhase = loadingPhases.allCases.randomElement() ?? .ld1
+
+            showText = false
+            loading = false
+            brightness = false
+            
+            withAnimation {
+                showText = true
+                loading = true
+                brightness = true
+            }
+        }
+    }
+}
+
+#Preview {
+    LoadingView()
+}
