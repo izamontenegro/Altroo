7750159b4aa3a9e9013f948cfebb6c3b84154b15
RaissaParente <raissabparente@gmail.com>
2025-11-06 18:14:49 -0300

    feat: implemented time filters to report

--- Altroo/Features/Main/Analysis/DailyReportViewModel.swift
+++ Altroo/Features/Main/Analysis/DailyReportViewModel.swift
@@ -15,17 +15,17 @@ struct CategoryInfo {
 
 
 class DailyReportViewModel: ObservableObject {
-    
+    //MARK: - DEPENDENCIES
     let userService: UserServiceProtocol
     let careRecipientFacade: CareRecipientFacade
     var basicNeedsFacade: BasicNeedsFacade
     var routineActivitiesFacade: RoutineActivitiesFacade
     
+    //MARK: - RECORDS
     var hydrationRecords: [ReportItem] = []
     var feedingRecords: [ReportItem] = []
     var stoolRecords: [ReportItem] = []
     var urineRecords: [ReportItem] = []
-    
     var tasksRecords: [ReportItem] = []
     var symptomRecords: [ReportItem] = []
     
@@ -39,6 +39,34 @@ class DailyReportViewModel: ObservableObject {
            ]
            .filter { !$0.reports.isEmpty }
        }
+    
+    //MARK: - FILTERS
+    @Published var startDate: Date = Date()
+    @Published var endDate: Date?
+    @Published var startTime: Date = Calendar.current.startOfDay(for: .now)
+    @Published var endTime: Date = {
+        let startOfTomorrow = Calendar.current.startOfDay(for: Calendar.current.date(byAdding: .day, value: 1, to: .now)!)
+        return startOfTomorrow.addingTimeInterval(-1)
+    }()
+    
+    var timeRange: ClosedRange<Date> {
+        guard let fullStart = combine(date: startDate, withTimeFrom: startTime) else {
+            return Date.now...Date.now
+        }
+
+        // periodical report
+        if let endDate,
+           let fullEnd = combine(date: endDate, withTimeFrom: endTime) {
+            return fullStart...fullEnd
+        } else {
+            //daily report -> has no endday, but may have endtime
+            guard let fallbackEnd = combine(date: startDate, withTimeFrom: endTime) else {
+                return fullStart...fullStart
+            }
+            return fullStart...fallbackEnd
+        }
+                
+    }
 
     init(basicNeedsFacade: BasicNeedsFacade, userService: UserServiceProtocol, careRecipientFacade: CareRecipientFacade, routineActivitiesFacade: RoutineActivitiesFacade) {
         self.basicNeedsFacade = basicNeedsFacade
@@ -57,28 +85,44 @@ class DailyReportViewModel: ObservableObject {
         guard let currentCareRecipient else { return }
                 
         hydrationRecords = basicNeedsFacade.fetchHydrations(for: currentCareRecipient)
+            .filter { timeRange.contains($0.reportTime ?? .now)}
             .map { .hydration($0) }
+        
         feedingRecords = basicNeedsFacade.fetchFeedings(for: currentCareRecipient)
+            .filter { timeRange.contains($0.reportTime ?? .now)}
             .map { .feeding($0) }
 
         stoolRecords = basicNeedsFacade.fetchStools(for: currentCareRecipient)
+            .filter { timeRange.contains($0.reportTime ?? .now)}
             .map { .stool($0) }
 
         urineRecords = basicNeedsFacade.fetchUrines(for: currentCareRecipient)
+            .filter { timeRange.contains($0.reportTime ?? .now)}
             .map { .urine($0) }
         
         tasksRecords = routineActivitiesFacade.fetchAllInstanceRoutineTasks(from: currentCareRecipient)
             .filter { $0.isDone }
+            .filter { timeRange.contains($0.reportTime ?? .now)}
             .map { .task($0) }
 
         symptomRecords = careRecipientFacade.fetchAllSymptoms(from: currentCareRecipient)
+            .filter { timeRange.contains($0.reportTime ?? .now)}
             .map { .symptom($0) }
+    }
+    
+    //MARK: - UTIL
+    func combine(date datePart: Date, withTimeFrom timePart: Date, using calendar: Calendar = .current) -> Date? {
+        let dateComponents = calendar.dateComponents([.year, .month, .day], from: datePart)
+        let timeComponents = calendar.dateComponents([.hour, .minute, .second], from: timePart)
         
+        var combined = DateComponents()
+        combined.year = dateComponents.year
+        combined.month = dateComponents.month
+        combined.day = dateComponents.day
+        combined.hour = timeComponents.hour
+        combined.minute = timeComponents.minute
+        combined.second = timeComponents.second
         
-//        print("Agua: \(hydrationRecords)")
-//        print("Xixi: \(urineRecords)")
-//        print("Coco: \(stoolRecords)")
-        print("Sintomas: \(symptomRecords.map({$0.base.reportTitle}))")
-//        print("Gagau: \(tasksRecords)")
+        return calendar.date(from: combined)
     }
 }
