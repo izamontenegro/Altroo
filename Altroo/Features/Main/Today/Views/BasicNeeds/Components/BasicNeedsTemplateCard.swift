24eec2f4f971309ada4d69ac2ee5d6e9de3c12a3
Izadora Montenegro <125757162+izamontenegro@users.noreply.github.com>
2025-11-17 11:16:16 -0300

    feat: implement the template card button in swiftui

---
+++ Altroo/Features/Main/Today/Views/BasicNeeds/Components/BasicNeedsTemplateCard.swift
@@ -0,0 +1,63 @@
+//
+//  BasicNeedsTemplateCard.swift
+//  Altroo
+//
+//  Created by Izadora de Oliveira Albuquerque Montenegro on 17/11/25.
+//
+
+import SwiftUI
+
+struct BasicNeedsTemplateCard: View {
+    let imageName: String
+    let subtitle: String
+    let title: String
+    
+    let action: (() -> Void)
+    
+    @State var isSelected: Bool = false
+
+    var body: some View {
+        Button(action: {
+            action()
+            isSelected.toggle()
+        }, label: {
+            VStack(alignment: .leading, spacing: 5) {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(5.4)
+                    .frame(width: 110)
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(subtitle.uppercased())
+                        .foregroundStyle(.black20)
+                        .font(.callout)
+                    Text(title)
+                        .font(.callout)
+                        .foregroundStyle(.blue20)
+                        .fontWeight(.medium)
+                }
+                
+                Spacer()
+            }
+        })
+        .buttonStyle(.plain)
+        .padding(8)
+        .background {
+            Color.blue80
+        }
+        .frame(maxHeight: 160)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(isSelected ? Color.blue : Color.clear, lineWidth: 2)
+        )
+    }
+}
+
+#Preview {
+    BasicNeedsTemplateCard(imageName: "MealIllustration_Lunch", subtitle: "Subtitle", title: "Title", action: {
+        print("button tapped")
+    })
+}
+
