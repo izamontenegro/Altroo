4253fcc1fa5cc4f45f0b3cc7f2f7624ecbbd2a09
Layza Carneiro <layza.mrcarneiro@gmail.com>
2025-11-24 20:21:38 -0300

    feat: finish animations in loading receive patient view

--- Altroo/App/SceneDelegate.swift
+++ Altroo/App/SceneDelegate.swift
@@ -1,62 +1,62 @@
-////
-////  SceneDelegate.swift
-////  Altroo
-////
-////  Created by Izadora de Oliveira Albuquerque Montenegro on 18/09/25.
-////
 //
-//import UIKit
-//import CloudKit
+//  SceneDelegate.swift
+//  Altroo
 //
-//class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-//    
-//    var window: UIWindow?
-//    var appCoordinator: AppCoordinator?
-//    
-//    func scene(_ scene: UIScene,
-//               willConnectTo session: UISceneSession,
-//               options connectionOptions: UIScene.ConnectionOptions) {
-//        
-//        guard let windowScene = scene as? UIWindowScene else { return }
-//        
-//        let window = UIWindow(windowScene: windowScene)
-//        let navigation = UINavigationController()
-//        
-//        let appCoordinator = AppCoordinator(rootNavigation: navigation)
-//                
-//        window.rootViewController = navigation
-//        window.makeKeyAndVisible()
-//        window.overrideUserInterfaceStyle = .light
-//        
-//        self.window = window
-//        self.appCoordinator = appCoordinator
-//        
-//        Task {
-//            await appCoordinator.start()
-//        }
-//    }
-//    
-//    func windowScene(_ windowScene: UIWindowScene,
-//                     userDidAcceptCloudKitShareWith cloudKitShareMetadata: CKShare.Metadata) {
+//  Created by Izadora de Oliveira Albuquerque Montenegro on 18/09/25.
 //
-//        let shareStore = CoreDataStack.shared.sharedPersistentStore
-//        let persistentContainer = CoreDataStack.shared.persistentContainer
-//
-//        persistentContainer.acceptShareInvitations(from: [cloudKitShareMetadata],
-//                                                   into: shareStore) { _, error in
-//            if let error {
-//                print("Error accepting CloudKit Share: \(error)")
-//            } else {
-//                print("CloudKit Share invitation successfully accepted..")
-//                
-//                DispatchQueue.main.async {
-//                    self.appCoordinator?.receivedPatientViaShare = true
-//                    
-//                    Task {
-//                        await self.appCoordinator?.start()
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+
+import UIKit
+import CloudKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let navigation = UINavigationController()
+        
+        let appCoordinator = AppCoordinator(rootNavigation: navigation)
+                
+        window.rootViewController = navigation
+        window.makeKeyAndVisible()
+        window.overrideUserInterfaceStyle = .light
+        
+        self.window = window
+        self.appCoordinator = appCoordinator
+        
+        Task {
+            await appCoordinator.start()
+        }
+    }
+    
+    func windowScene(_ windowScene: UIWindowScene,
+                     userDidAcceptCloudKitShareWith cloudKitShareMetadata: CKShare.Metadata) {
+
+        let shareStore = CoreDataStack.shared.sharedPersistentStore
+        let persistentContainer = CoreDataStack.shared.persistentContainer
+
+        persistentContainer.acceptShareInvitations(from: [cloudKitShareMetadata],
+                                                   into: shareStore) { _, error in
+            if let error {
+                print("Error accepting CloudKit Share: \(error)")
+            } else {
+                print("CloudKit Share invitation successfully accepted..")
+                
+                DispatchQueue.main.async {
+                    self.appCoordinator?.receivedPatientViaShare = true
+                    
+                    Task {
+                        await self.appCoordinator?.start()
+                    }
+                }
+            }
+        }
+    }
+}
--- Altroo/Shared/Components/Loading/LoadingReceivePatient.swift
+++ Altroo/Shared/Components/Loading/LoadingReceivePatient.swift
@@ -9,29 +9,31 @@ import SwiftUI
 
 struct LoadingReceivePatientView: View {
     let logo: Image
-    let duration: Double
+    let duration: Double = 1.5
+    let pause: Double = 0.5
     let isSyncing: Bool
 
-    @State private var progress: CGFloat = 0
-    @State private var textOpacity: Double = 1
-    @State private var showSyncing: Bool = false
     @State private var rotation: Double = 0
+    @State private var textOpacity: Double = 1
+    @State private var progress: CGFloat = 0
+    @State private var showSyncing: Bool = false
     @State private var syncingAnimationOffset: CGFloat = -250
+    @State private var brightness = false
 
     var body: some View {
         ZStack {
             Color.white.opacity(0.7)
             
-            VStack(spacing: 40) {
+            VStack(spacing: 14) {
+                Spacer()
+                
                 logo
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 120, height: 120)
+                    .frame(width: 165, height: 165)
                     .rotationEffect(.degrees(rotation))
                     .onAppear {
-                        withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)) {
-                            rotation = 360
-                        }
+                        startRotationStep()
                     }
                 
                 Text(showSyncing ? "Sincronizando..." : "Carregando...")
@@ -40,7 +42,7 @@ struct LoadingReceivePatientView: View {
                     .foregroundColor(.blue30)
                     .opacity(textOpacity)
                     .onAppear {
-                        withAnimation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true)) {
+                        withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: true)) {
                             textOpacity = 0.3
                         }
                     }
@@ -48,12 +50,12 @@ struct LoadingReceivePatientView: View {
                 ZStack(alignment: .leading) {
                     RoundedRectangle(cornerRadius: 12)
                         .frame(height: 20)
-                        .foregroundColor(Color.blue.opacity(0.3))
+                        .foregroundColor(.blue60)
                     
                     if isSyncing {
                         RoundedRectangle(cornerRadius: 12)
                             .frame(width: 100, height: 20)
-                            .foregroundColor(.blue60)
+                            .foregroundColor(.blue30)
                             .offset(x: syncingAnimationOffset)
                             .onAppear {
                                 let animation = Animation.linear(duration: 1).repeatForever(autoreverses: false)
@@ -69,6 +71,8 @@ struct LoadingReceivePatientView: View {
                     }
                 }
                 .frame(maxWidth: 250)
+                
+                Spacer()
             }
             .onAppear {
                 if !isSyncing {
@@ -77,9 +81,34 @@ struct LoadingReceivePatientView: View {
                     showSyncing = true
                 }
             }
-            .padding()
+            .padding(.bottom, 100)
+
+            VStack {
+                Spacer()
+                Ellipse()
+                    .fill(.blue60)
+                    .blur(radius: brightness ? 70 : 100)
+                    .frame(width: UIScreen.main.bounds.width * 0.8, height: brightness ? 130 : 90)
+                    .onAppear {
+                        withAnimation(.easeInOut(duration: 2).repeatForever(autoreverses: true)) {
+                            brightness.toggle()
+                        }
+                    }
+            }
         }
         .ignoresSafeArea()
+    }
+
+    private func startRotationStep() {
+        func rotateOnce() {
+            withAnimation(.linear(duration: duration)) {
+                rotation += 360
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration + pause) {
+                rotateOnce()
+            }
+        }
+        rotateOnce()
     }
 
     private var progressBarWidth: CGFloat {
@@ -87,29 +116,14 @@ struct LoadingReceivePatientView: View {
     }
 
     private func animateProgress() {
-        withAnimation(.linear(duration: duration)) {
+        withAnimation(.linear(duration: duration * 5)) {
             progress = 1
         }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration * 5) {
             if isSyncing {
                 showSyncing = true
             }
-        }
-    }
-}
-
-import SwiftUI
-
-@main
-struct LoadingTestApp: App {
-    var body: some Scene {
-        WindowGroup {
-            LoadingReceivePatientView(
-                logo: Image(systemName: "accessibility"),
-                duration: 5,
-                isSyncing: false
-            )
         }
     }
 }
